ee0ae1ee4ac0bdb78c7bfb90189ea20f9c315152
MyDataKitchen <mylearning.jiayong@gmail.com>
2022-06-16 15:13:52 +0800

    [CRAWLER_SCHEDULAR][PYTHON] scheduling crawler work

--- crawler/scheduler.py
+++ crawler/scheduler.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
 
     schedule.every(1).minutes.do(do_youbike_crawling)
     schedule.every(10).minutes.do(do_wheather_crawling)
+
     while True:
         schedule.run_pending()
         time.sleep(1)
