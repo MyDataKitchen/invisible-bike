f04c322733904af23f1d569091fd82c3ef4b5fc6
MyDataKitchen <mylearning.jiayong@gmail.com>
2022-06-17 15:37:21 +0800

    [ADD_PRECIPTATION_CRAWLER_SCHEDULING][PYTHON] Add precipitation crawler scheduling

--- crawler/scheduler.py
+++ crawler/scheduler.py
@@ -5,12 +5,16 @@ import time
 def do_youbike_crawling():
     subprocess.run(["python3", "youbike_crawler.py"])
 
+def do_precipitation_crawling():
+    subprocess.run(["python3", "precipitation_crawler.py"])
+
 def do_wheather_crawling():
     subprocess.run(["python3", "wheather_crawler.py"])
 
 if __name__ == '__main__':
 
     schedule.every(1).minutes.do(do_youbike_crawling)
+    schedule.every(10).minutes.do(do_precipitation_crawling)
     schedule.every(10).minutes.do(do_wheather_crawling)
 
     while True:
