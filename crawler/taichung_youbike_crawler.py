8c6368cfe080acf81ffd6bcb90b395f32678262e
Alfred <mylearning.jiayong@gmail.com>
2022-06-25 17:29:31 +0800

    [MODIFIED_TIME_FUNCTION][PYTHON] Modified datetime function

--- crawler/taichung_youbike_crawler.py
+++ crawler/taichung_youbike_crawler.py
@@ -2,7 +2,7 @@ from s3 import put_data_to_s3
 from datetime import datetime as dt
 from datetime import timezone, timedelta
 from dotenv import load_dotenv
-from model import get_latest_log, insert_crawler_log
+from model import insert_crawler_log
 import requests
 import json
 import os
@@ -21,7 +21,7 @@ FILE_NAME = os.getenv('TAICHUNG_YOUBIKE_FILE_NAME')
 def request_data(url):
     respone = requests.get(url)
     data = respone.json()
-    updated_time = datetime()
+    updated_time = datetime().strftime("%Y-%m-%d %H:%M:S")
     size = sys.getsizeof(json.dumps(data))
     size = round( size / 1024, 0)
     return data, updated_time, size
@@ -30,8 +30,8 @@ def request_data(url):
 def datetime():
     tz = timezone(timedelta(hours=+8))
     now = dt.now(tz)
-    dt_string = now.strftime("%Y%m%d_%H:%M")
-    return dt_string
+
+    return now
 
 
 def insert_data_to_s3(bucket, filename, data):
@@ -40,7 +40,7 @@ def insert_data_to_s3(bucket, filename, data):
 
 
 if __name__ == '__main__':
-    date_time = datetime()
+    date_time = datetime().strftime("%Y%m%d_%H:%M")
     start = time.time()
     data, updated_time, size = request_data(YOUBIKE_URL)
     end = time.time()
@@ -55,16 +55,14 @@ if __name__ == '__main__':
             filename = f"{date_time}_{FILE_NAME}.json"
             end = time.time()
             execution_time = end - start
-            insert_crawler_log(SQL_TABLE,
-                               (filename, updated_time, len(data), size, response_time, execution_time, 0, None))
+            insert_crawler_log(SQL_TABLE, (filename, updated_time, len(data['retVal']), size, response_time, execution_time, 0, None))
 
         else:
             filename = f"{date_time}_{FILE_NAME}.json"
             aws_response = insert_data_to_s3(S3_BUCKET, S3_DIRECTORY_PATH + filename, data)
             end = time.time()
             execution_time = end - start
-            insert_crawler_log(SQL_TABLE,
-                               (filename, updated_time, len(data), size, response_time, execution_time, 1, json.dumps(aws_response)))
+            insert_crawler_log(SQL_TABLE, (filename, updated_time, len(data['retVal']), size, response_time, execution_time, 1, json.dumps(aws_response)))
 
             with open("temp/temp_data.json", "w") as outfile:
                 json_object = json.dumps(data)
@@ -77,8 +75,7 @@ if __name__ == '__main__':
         aws_response = insert_data_to_s3(S3_BUCKET, S3_DIRECTORY_PATH + filename, data)
         end = time.time()
         execution_time = end - start
-        insert_crawler_log(SQL_TABLE,
-                           (filename, updated_time, len(data), size, response_time, execution_time, 1, json.dumps(aws_response)))
+        insert_crawler_log(SQL_TABLE, (filename, updated_time, len(data['retVal']), size, response_time, execution_time, 1, json.dumps(aws_response)))
 
         with open("temp/temp_data.json", "w") as outfile:
             json_object = json.dumps(data)
